003cca5b59c1931876414b84cd8339b9c2def37d
CCEG24 <chenyi.ge2013@gmail.com>
2025-01-01 11:39:49 +0000

    Deleted caching comment

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,8 +6,6 @@ from django.views import generic
 from .models import Choice, Question
 from django.views.decorators.cache import cache_page
 
-# @cache_page(60 * 15)
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
