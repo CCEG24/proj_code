d6dd275adf7f6466665803f2c1e168a4ef3cc60d
Chenyi <chenyi.ge2013@gmail.com>
2025-06-22 18:53:45 +0100

    small bug fix

--- mysite/question_bot/urls.py
+++ mysite/question_bot/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'question_bot'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('question_bot/', include('question_bot.urls')),
 ]
