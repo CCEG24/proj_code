6595c36f83b34ec22aacc8388612c4b4a10113e9
Chenyi <chenyi.ge2013@gmail.com>
2025-07-10 11:22:57 +0100

    fixed views.py

--- mysite/tetris/views.py
+++ mysite/tetris/views.py
@@ -4,3 +4,6 @@ from .models import HighScore
 def leaderboard(request):
     scores = HighScore.objects.order_by('-score')[:10]
     return render(request, 'tetris/leaderboard.html', {'scores': scores})
+
+def game(request):
+    return render(request, 'tetris/game.html')
