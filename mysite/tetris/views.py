dac3b01d43877962290b145fe043714b5077c597
Chenyi <chenyi.ge2013@gmail.com>
2025-07-10 11:22:20 +0100

    fixed leaderboard

--- mysite/tetris/views.py
+++ mysite/tetris/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import HighScore
 
-# Create your views here.
+def leaderboard(request):
+    scores = HighScore.objects.order_by('-score')[:10]
+    return render(request, 'tetris/leaderboard.html', {'scores': scores})
