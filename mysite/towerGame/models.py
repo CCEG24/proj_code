5714872f51b38cbe6f9c559920e10510d17cd093
Chenyi Ge <chenyi.ge2013@gmail.com>
2026-04-14 10:54:44 +0100

    Add wasted_moves model and update ordering

--- mysite/towerGame/models.py
+++ mysite/towerGame/models.py
@@ -5,10 +5,11 @@ class TowerGameScore(models.Model):
     player_name = models.CharField(max_length=100, default="Anonymous")
     moves = models.IntegerField()
     difficulty = models.IntegerField(default=3)
+    wasted_moves = models.IntegerField(default=2)
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering = ['moves', 'difficulty']
+        ordering = ['wasted_moves', 'difficulty']
         verbose_name = "Tower Game Score"
         verbose_name_plural = "Tower Game Scores"
     
